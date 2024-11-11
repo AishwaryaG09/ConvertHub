5372d9ce7d568a3b47d93462f8035567eb69f540
Viraj Ghorpade <viraj.ghorpade9767@gmail.com>
2024-11-11 23:23:33 +0530

    feat: ConvertHub new code base

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Router from "./navigation/Router";
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Router />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Calculators_SubComponent/HikeCalculator.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import styled from "styled-components";
+import formatNumber from "../../utils/formatNumber";
+
+const StyledH1 = styled.h1`
+  padding: 8px 8px;
+  color: #2a52be;
+`;
+const StyleDiv = styled.div`
+  padding: 12px;
+  width: 60%;
+`;
+const StyleButton = styled.button`
+  border-radius: 4px;
+  background: #2a52be;
+  color: #ffffff;
+  border: 1px solid #000000;
+  margin-right: 14px;
+  padding: 8px
+`;
+
+const StyledP = styled.p`
+  margin-bottom: 5px;
+  color: #000000;
+`;
+
+const StyledP2 = styled.p`
+  font-size: 24px;
+  color: #000000;
+`;
+
+const HikeCalculator = () => {
+  const [percent, setPercent] = useState();
+  const [currentSalary, setCurrentSalary] = useState();
+  const [increaseSalary, setIncreaseSalary] = useState();
+
+  const findIncreaseSalary = () => {
+    const increaseSalary = (currentSalary * percent) / 100;
+    const TotalAmount = increaseSalary + Number(currentSalary);
+    setIncreaseSalary(TotalAmount);
+  };
+
+  const reset = () => {
+    setPercent();
+    setCurrentSalary();
+    setIncreaseSalary();
+  };
+
+  console.log("currentSalary---", currentSalary);
+  console.log("percent---", percent);
+  return (
+    <>
+      <StyledH1>Find new increased salary by Percentage</StyledH1>
+      <StyleDiv>
+        <StyledP>Current salary: </StyledP>
+        <input
+          type="number"
+          id="currentSalary"
+          name="currentSalary"
+          style={{ width: "100%" }}
+          value={Number(currentSalary) !== 0 && Number(currentSalary)}
+          onChange={(e) => {
+            console.log("e.target.value--", e.target.value);
+            setCurrentSalary(e.target.value);
+          }}
+        />
+      </StyleDiv>
+      <StyleDiv>
+        <StyledP>Increase Percentage: </StyledP>
+        <input
+          type="number"
+          id="percent"
+          name="percent"
+          style={{ width: "100%" }}
+          value={Number(percent) !== 0 && Number(percent)}
+          onChange={(e) => {
+            console.log("e.target.value--", e.target.value);
+            setPercent(e.target.value);
+          }}
+        />
+      </StyleDiv>
+      <StyleDiv>
+        <StyleButton
+          onClick={() => {
+            findIncreaseSalary();
+          }}
+        >
+          Calculate
+        </StyleButton>
+        <StyleButton
+          onClick={() => {
+            reset();
+          }}
+        >
+          Reset
+        </StyleButton>
+      </StyleDiv>
+      <StyleDiv>
+        <StyledP2>
+          New increased Salary : {increaseSalary && formatNumber(increaseSalary)}
+        </StyledP2>
+      </StyleDiv>
+    </>
+  );
+};
+
+export default HikeCalculator;
